374e195b4e36f05ab651176347c711f8a08f2d4f
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-10-12 08:48:31 +0200

    Add code from Dart code to capture coverage

--- test/index.ts
+++ test/index.ts
@@ -9,8 +9,23 @@
 // host can call to run the tests. The test runner is expected to use console.log
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
-
+const fs = require('fs');
 var testRunner = require('vscode/lib/testrunner');
+const onExit = require('signal-exit'); // tslint:disable-line:no-var-requires
+
+// Ensure we write coverage on exit.
+declare const __coverage__: any;
+onExit(() => {
+	// Unhandled exceptions here seem to hang, but console.error+process.exit do not! ¯\_(ツ)_/¯
+	try {
+		if (typeof __coverage__ !== 'undefined' && typeof process.env.COVERAGE_OUTPUT !== 'undefined' && process.env.COVERAGE_OUTPUT) {
+			fs.writeFileSync(process.env.COVERAGE_OUTPUT, JSON.stringify(__coverage__));
+		}
+	} catch (e) {
+		console.error(e);
+		process.exit(1);
+	}
+});
 
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
